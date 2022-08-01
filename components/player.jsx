ce5ff9aecbba591f4ca54fa5ca4e17b7914d2a45
steven <devil.jaws@gmail.com>
2022-08-01 23:40:04 +0300

    a

--- components/player.jsx
+++ components/player.jsx
@@ -18,7 +18,6 @@ import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/song-atom";
 import useSongInfo from "../hooks/useSongInfo";
 import useSpotify from "../hooks/useSpotify";
-import millisToMinutesAndSeconds from "../lib/time";
 
 export default function Player() {
   const spotifyApi = useSpotify();
@@ -32,7 +31,7 @@ export default function Player() {
 
   console.log(songInfo);
 
-  const fetchCurrentSong = () => {
+  const fetchCurrentSong = async () => {
     console.log("Fetching the current song...");
     if (!songInfo) {
       spotifyApi.getMyCurrentPlayingTrack().then((data) => {
@@ -71,9 +70,7 @@ export default function Player() {
       if (data.body?.is_playing) {
         spotifyApi.pause();
         setIsPlaying(false);
-        console.log(
-          `Paused at ${millisToMinutesAndSeconds(data.body.progress_ms)}`
-        );
+        console.log(`Paused at ${millisToSeconds(data.body.progress_ms)}`);
       } else {
         spotifyApi.play();
         setIsPlaying(true);
@@ -86,7 +83,7 @@ export default function Player() {
       fetchCurrentSong();
       setVolume(80);
     }
-  }, [currentTrackId, spotifyApi, session]);
+  }, [currentTrackId, spotifyApi]);
 
   const debouncedAdjustVolume = useCallback(
     debounce((volume) => {
@@ -105,21 +102,25 @@ export default function Player() {
 
   return (
     <>
-      <div className="h-24 bg-gradient-to-b from-gray-900 to-black text-white grid grid-cols-3 text-sm md:text-base px-2 md:px-8">
-        <div className="flex items-center space-x-4">
+      <div className="bg-gradient-to-b from-gray-900 to-black text-white flex items-center justify-between h-24 text-sm px-4 md:text-base py-0 md:px-8">
+        <div className="flex basis-full justify-start items-center space-x-4">
           <img
-            className="hidden md:inline h-12 w-12"
+            className="hidden md:inline h-12 w-12 cursor-pointer"
             src={songInfo.album.images?.[0].url}
             alt=""
           />
           <div>
-            <h3>{songInfo?.name}</h3>
-            <p className="text-sm text-gray-500">
-              {songInfo?.artists?.[0]?.name}
+            <h3 className="text-sm hover:underline cursor-pointer">
+              {songInfo?.name}
+            </h3>
+            <p className="text-xs text-gray-500 cursor-pointer">
+              {songInfo?.artists?.map((art) => (
+                <p className="hover:text-white hover:underline">{art.name}</p>
+              ))}
             </p>
           </div>
         </div>
-        <div className="flex items-center justify-evenly">
+        <div className="flex basis-full flex-grow items-center justify-start w-full space-x-8">
           <SwitchHorizontalIcon className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out" />
           <RewindIcon
             onClick={goToPrev}
@@ -138,9 +139,10 @@ export default function Player() {
             onClick={goToNext}
             className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
           />
-          <ReplyIcon className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out" />
-        </div>
 
+          <ReplyIcon className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out" />
+          {/* playback bar  */}
+        </div>
         <div className="flex items-center space-x-3 md:space-x-4 justify-end p-5">
           <VolumeDownIcon
             className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
