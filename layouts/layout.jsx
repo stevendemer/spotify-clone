08293b6aee49b40d68aabc93ec47319f5011596c
steven <devil.jaws@gmail.com>
2022-08-04 20:51:22 +0300

    Checkpoint sheesh

--- layouts/layout.jsx
+++ layouts/layout.jsx
@@ -8,9 +8,7 @@ export default function Layout({ hasSidebar, children, ...restProps }) {
         {hasSidebar && <Sidebar />}
         {children}
       </main>
-      <div className="w-full z-50 absolute bottom-0">
-        {hasSidebar && <Player />}
-      </div>
+      <div className="w-full absolute bottom-0">{hasSidebar && <Player />}</div>
     </div>
   );
 }
